ef05586b4f583aeb41b9fdb0a207816772272619
Ondřej John <ondrej.john@ackee.cz>
2021-04-01 15:55:52 +0200

    Add Slack notification message script

---
+++ slack/prepare_slack_msg.sh
@@ -0,0 +1,15 @@
+LIB_PROP_FILE="lib.properties"
+VERSION_NAME=`cat $LIB_PROP_FILE | grep VERSION_NAME | cut -d = -f 2`
+SITE_URL=`cat $LIB_PROP_FILE | grep SITE_URL | cut -d = -f 2`
+LIB_NAME=`echo $SITE_URL | cut -d / -f 5`
+GIT_CHANGELOG=`git log $(git describe --tags --abbrev=0)..HEAD --oneline --no-decorate | cut -d ' ' -f 2- | tail -n +3`
+export SLACK_MSG_TITLE="$LIB_NAME $VERSION_NAME released :tada:"
+export SLACK_MSG="Changes:
+$GIT_CHANGELOG
+
+See more at $SITE_URL."
+SLACK_MSG="${SLACK_MSG//'%'/'%25'}"
+SLACK_MSG="${SLACK_MSG//$'\n'/'%0A'}"
+SLACK_MSG="${SLACK_MSG//$'\r'/'%0D'}"
+echo "::set-output name=SLACK_MSG::$(echo "$SLACK_MSG")"
+echo "::set-output name=SLACK_MSG_TITLE::$(echo "$SLACK_MSG_TITLE")"
