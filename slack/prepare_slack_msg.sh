382209108b98cff33656aba3f6930d756a51a058
Ondřej John <ondrej.john@ackee.cz>
2021-04-14 09:57:27 +0200

    Exclude merge commits from changelog

--- slack/prepare_slack_msg.sh
+++ slack/prepare_slack_msg.sh
@@ -2,7 +2,7 @@ LIB_PROP_FILE="lib.properties"
 VERSION_NAME=`cat $LIB_PROP_FILE | grep VERSION_NAME | cut -d = -f 2`
 SITE_URL=`cat $LIB_PROP_FILE | grep SITE_URL | cut -d = -f 2`
 LIB_NAME=`echo $SITE_URL | cut -d / -f 5`
-GIT_CHANGELOG=`git log $(git describe --tags --abbrev=0 --always $(git rev-list --tags --skip=1 --max-count=1))..HEAD --oneline --no-decorate | cut -d ' ' -f 2- | tail -n +2`
+GIT_CHANGELOG=`git log $(git describe --tags --abbrev=0 --always $(git rev-list --tags --skip=1 --max-count=1))..HEAD --oneline --no-decorate | cut -d ' ' -f 2- | grep -i -v "^Merge"`
 export SLACK_MSG_TITLE="$LIB_NAME $VERSION_NAME released :tada:"
 export SLACK_MSG="Changes:
 $GIT_CHANGELOG
